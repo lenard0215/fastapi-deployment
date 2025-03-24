39dd45f3a01b073c568a2f9a1979d365c6461a24
lenard0215 <scpalmer2677@gmail.com>
2025-03-24 08:16:00 -0700

    changge in the main.py file

--- app/main.py
+++ app/main.py
@@ -48,10 +48,10 @@ app.include_router(login.router)
 app.include_router(comments.router)
 app.include_router(dislikes.router)
 
-if __name__ == '__main__':
-    uvicorn.run('app.main:app', 
-                host= "0.0.0.0",
-                port = 8080,
-                log_level = "debug",
-                reload = True)
+#if __name__ == '__main__':
+    #uvicorn.run('app.main:app', 
+                #host= "0.0.0.0",
+                #port = 8080,
+                #log_level = "debug",
+                #reload = True)
 
